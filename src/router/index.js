7a4e754be82eb4fdb1b93256d3a60d37b65e9522
dragonGTR <77241091+dragonGTR@users.noreply.github.com>
2021-06-07 11:56:18 +0530

    First Commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home'
+import AddPost from '../views/AddPost.vue'
+import Detail from '../views/Detail.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +13,29 @@ const routes = [
     component: Home
   },
   {
+    path: '/add-post',
+    name: 'add-post',
+    component: AddPost
+  },
+  {
+    path: '/movieDetail/:id',
+    name: 'Detail',
+    component:Detail
+  },
+  {
+    path:'/updateMovie/:id',
+    name:'updateMovie',
+    component :()=>import('../views/Update')
+  },
+  {
     path: '/about',
-    name: 'About',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col sm="4" class="pa-3" v-for="movie in movieList" :key="movie._id">
+        <v-card
+          class="pa-1"
+          :to="{ name: 'Detail', params: { id: movie._id } }"
+        >
+          <v-img height="230" :src="`/${movie.image}`"> </v-img>
+          <v-btn class="ml-4 mt-3" small outlined color="red">{{
+            movie.category
+          }}</v-btn>
+          <v-card-title class="headline">
+            {{ movie.title }}
+          </v-card-title>
+          <v-card-text class="py-0">
+            <p>{{ movie.content.substring(0, 30) }}....</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Api from "./axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      movieList: [],
+    };
+  },
+  async created() {
+    this.movieList = await Api.getAllPost();
+  },
+};
 </script>
